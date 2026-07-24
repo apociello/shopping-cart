d0cabcb147305b0c8ca9f80a3af4d0ef8d546e1c
pociello <asierpociello@gmail.com>
2026-07-24 19:07:21 +0200

    feat: remodel homePage (matching header)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,8 @@
-import myImage from '../assets/images/img1.webp';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.imgDiv}>
-        <img src={myImage} className={styles.img} alt="" />
-      </div>
 
       <div className={styles.info}>
         <h1 className={styles.h1}>Everyday essentials</h1>
