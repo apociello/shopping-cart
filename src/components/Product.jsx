b8d65b290d1ddc8190fc8e90b7e1ba3e56d01f95
pociello <asierpociello@gmail.com>
2026-07-26 11:17:25 +0200

    feat: add style to product cards

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,17 @@
-export default function Product({image, title, price}) {
-    return (
-        <div>
-            <img src={image} alt="" />
-            <p>{title}</p>
-            <p>{price} €</p>
-            <button>Add to cart</button>
-        </div>
-    )
+import styles from '../styles/Product.module.css';
+
+export default function Product({ image, title, price }) {
+  return (
+    <div className={styles.product}>
+      <div>
+        <img src={image} className={styles.image} alt="" />
+        <p className={styles.title}>{title}</p>
+        <p className={styles.price}>{price} €</p>
+      </div>
+
+      <div>
+        <button className={styles.button}>Add to cart</button>
+      </div>
+    </div>
+  );
 }
