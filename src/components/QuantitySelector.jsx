41e1b043e104408d42809d3294301eacb5a5355a
pociello <asierpociello@gmail.com>
2026-07-27 10:37:22 +0200

    feat: add lucide icons to quantity selector

--- src/components/QuantitySelector.jsx
+++ src/components/QuantitySelector.jsx
@@ -1,13 +1,22 @@
+import { Trash, Minus, Plus } from 'lucide-react';
 import styles from '../styles/QuantitySelector.module.css';
 
 export default function QuantitySelector({ quantity, setQuantity }) {
   return (
     <div className={styles.wrapper}>
-      <button onClick={() => setQuantity(quantity - 1)}>
-        {quantity <= 1 ? 'X' : '-'}
+      <button
+        className={styles.button}
+        onClick={() => setQuantity(quantity - 1)}
+      >
+        {quantity <= 1 ? <Trash size={17} /> : <Minus size={17} />}
       </button>
       <p>{quantity}</p>
-      <button onClick={() => setQuantity(quantity + 1)}>+</button>
+      <button
+        className={styles.button}
+        onClick={() => setQuantity(quantity + 1)}
+      >
+        <Plus size={17} />
+      </button>
     </div>
   );
 }
