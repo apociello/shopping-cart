1f683458c5f8281f4e87a2941d71d190aec60a5d
pociello <asierpociello@gmail.com>
2026-07-28 11:57:07 +0200

    refactor: use array methods instead of for loops in cart page

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -5,29 +5,24 @@ import styles from '../styles/CartPage.module.css';
 export default function CartPage() {
   const { products, cartItems } = useOutletContext();
 
-  let fullInfoArray = [];
-
-  for (let item of cartItems) {
-    for (let product of products) {
-      if (item.id === product.id) {
-        fullInfoArray.push({
-          id: product.id,
-          image: product.image,
-          title: product.title,
-          price: product.price,
-          quantity: item.quantity,
-        });
-      }
-    }
-  }
+  const cartProducts = cartItems.map((item) => {
+    const product = products.find((product) => product.id === item.id);
+    return {
+      id: product.id,
+      image: product.image,
+      title: product.title,
+      price: product.price,
+      quantity: item.quantity,
+    };
+  });
 
   return (
     <main className={styles.main}>
       <div className={styles.items}>
-        {fullInfoArray.length < 1 ? (
+        {cartProducts.length < 1 ? (
           <h1 className={styles.h1}>No items in the cart!</h1>
         ) : (
-          fullInfoArray.map((product) => (
+          cartProducts.map((product) => (
             <CartItem
               key={product.id}
               quantity={product.quantity}
