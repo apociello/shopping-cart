081b53c196a5aa41abc8154aad725178dd2cf887
pociello <asierpociello@gmail.com>
2026-07-29 08:19:47 +0200

    feat: add order summary

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -16,24 +16,50 @@ export default function CartPage() {
     };
   });
 
+  const subtotal = cartProducts.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0,
+  );
+  const shipping = subtotal <= 29.99 ? 3.99 : 0;
+  const total = subtotal + shipping;
+
   return (
     <main className={styles.main}>
-      <div className={styles.items}>
-        {cartProducts.length < 1 ? (
-          <h1 className={styles.h1}>No items in the cart!</h1>
-        ) : (
-          cartProducts.map((product) => (
-            <CartItem
-              key={product.id}
-              id={product.id}
-              quantity={product.quantity}
-              image={product.image}
-              price={product.price}
-              title={product.title}
-            />
-          ))
-        )}
-      </div>
+      {cartProducts.length < 1 ? (
+        <h1 className={styles.h1}>No items in the cart!</h1>
+      ) : (
+        <>
+          <div className={styles.items}>
+            {cartProducts.map((product) => (
+              <CartItem
+                key={product.id}
+                id={product.id}
+                quantity={product.quantity}
+                image={product.image}
+                price={product.price}
+                title={product.title}
+              />
+            ))}
+          </div>
+
+          <div className={styles.orderSummary}>
+            <div>
+              <p>Subtotal </p>
+              <p>{subtotal} €</p>
+            </div>
+
+            <div>
+              <p>Shipping </p>
+              <p>{shipping} €</p>
+            </div>
+
+            <div>
+              <p>Total </p>
+              <p>{total} €</p>
+            </div>
+          </div>
+        </>
+      )}
     </main>
   );
 }
