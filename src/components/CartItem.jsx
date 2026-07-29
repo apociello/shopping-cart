ebe8b1f6c81c409385a5dcc70169526a37b79e63
pociello <asierpociello@gmail.com>
2026-07-29 09:58:43 +0200

    feat: improve cart item visual style & composition

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -4,10 +4,20 @@ import styles from '../styles/CartItem.module.css';
 export default function CartItem({ id, quantity, image, title, price }) {
   return (
     <div className={styles.item}>
-      <img src={image} className={styles.image} alt="" />
-      <p>{title}</p>
-      <QuantitySelector id={id} quantity={quantity} />
-      <p>{price * quantity} €</p>
+      <div className={styles.left}>
+        <img src={image} className={styles.image} alt="" />
+        <p className={styles.title}>{title}</p>
+      </div>
+
+      <div className={styles.right}>
+        <QuantitySelector
+          id={id}
+          quantity={quantity}
+          className={styles.cartWrapper}
+        />
+
+        <p>{price * quantity} €</p>
+      </div>
     </div>
   );
 }
